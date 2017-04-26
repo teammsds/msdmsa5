cacb57ac702c7581703ca9847178bcfdf557690c
TarunReddyBattula <tbattula@unomaha.edu>
2017-04-25 22:31:33 -0500

    Update index.blade.php

--- resources/views/schools/index.blade.php
+++ resources/views/schools/index.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-15 col-md-offset-0">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                <div class="pull-left" align="middle">
+                <div class="pull-left" align="left">
                     <h4 style= "color: black">Schools</h4>
                     </div>
                       <a href="{{url('/schools/create')}}" class="btn btn-primary btn-sm">Add New School</a>
